e13ca9a52f949fb8a8cff749ca05e647d22e5077
Suraj N. Kurapati <sunaku@gmail.com>
2009-10-03 14:09:59 -0700

    Version 0.0.1

--- lib/ember.rb
+++ lib/ember.rb
@@ -8,9 +8,9 @@ gem 'inochi', '~> 1'
 require 'inochi'
 
 Inochi.init :Ember,
-  :version => '0.0.0',
-  :release => '2009-05-02',
-  :website => 'http://snk.tuxfamily.org/lib/ember',
+  :version => '0.0.1',
+  :release => '2009-10-03',
+  :website => 'http://snk.tuxfamily.org/lib/ember/',
   :tagline => 'eRuby template processor'
 
 require 'ember/template'
