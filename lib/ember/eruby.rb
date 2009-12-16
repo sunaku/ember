85c4e84687ffe28f04738add9f87710efc5a7d7a
Suraj N. Kurapati <sunaku@gmail.com>
2009-12-15 21:11:56 -0800

    add ERubyDocument#to_a to get leaf nodes in input-occurence order

--- lib/ember/eruby.rb
+++ lib/ember/eruby.rb
@@ -4,6 +4,28 @@ require 'treetop'
 Treetop.load __FILE__.sub(/rb$/, 'treetop')
 
 module ERubyDocument
+  ##
+  # Returns an array of all content and directive nodes, in the
+  # order that they occur in the input, from this parse tree.
+  #
+  def to_a
+    list = []
+
+    visitor = lambda do |node|
+      if node.kind_of? ERubyContent or node.kind_of? ERubyDirective
+        list << node unless node.text_value.empty?
+        next # do not visit the children of this nonterminal
+      end
+
+      if node.nonterminal?
+        node.elements.each(&visitor)
+      end
+    end
+
+    visitor.call self
+
+    list
+  end
 end
 
 module ERubyContent
